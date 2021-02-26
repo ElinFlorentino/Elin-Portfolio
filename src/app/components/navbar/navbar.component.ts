fa2f2b0c1cb0c5b2b831110abd0bc0741bb63aec
ElinFlorentin <elinflorentino@gmai.com>
2021-02-26 19:38:10 -0400

    proyects page ready

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // navSlide() {
+  //   const slideBtn = <HTMLInputElement>document.getElementById('btn-navbar')!;
+  //   slideBtn.checked = false;
+  // }
+}
+
+window.onload = function () {
+  const links = document.getElementsByClassName("nav-link")!;
+  const slideCheck = <HTMLInputElement>document.getElementById('btn-navbar')!;
+  for(var i = 0; i < links.length;i++){
+    links[i].addEventListener('click',()=>{
+      slideCheck.checked = false;
+    })
   }
 
-}
+};
